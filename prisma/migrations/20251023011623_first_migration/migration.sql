e865653691dd1f071fbee958b440f8b89a2a650e
jgsertoli <jgsertoli@gmail.com>
2025-10-22 22:41:07 -0300

    feat: add PostgreSQL migration setup and update .gitignore

---
+++ prisma/migrations/20251023011623_first_migration/migration.sql
@@ -0,0 +1,10 @@
+-- CreateTable
+CREATE TABLE "Message" (
+    "id" SERIAL NOT NULL,
+    "author" TEXT NOT NULL,
+    "content" TEXT NOT NULL,
+    "authorToken" TEXT NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+
+    CONSTRAINT "Message_pkey" PRIMARY KEY ("id")
+);
